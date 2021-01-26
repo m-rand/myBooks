f3a62d54b9614a72b016ed20d82ba1e8e4fccfbb
Marcel Balas <marcel.balas@gmail.com>
2021-01-26 08:27:50 +0100

    Initial Commit

---
+++ Shared/myBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  myBooksApp.swift
+//  Shared
+//
+//  Created by Marcel Baláš on 26.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct myBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
